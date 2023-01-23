786d4fb7d3144928a345deea334bb7447dffae49
Sami Dahoux <samidahoux@gmail.com>
2023-01-23 19:18:49 +0100

    Add last month and backup options

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import threading
 import tkinter as tk
+import traceback
 from tkinter import messagebox
 
 from core import *
@@ -26,23 +27,28 @@ class App:
         self.window = Window(self.on_merge, self.on_synthetize, self.on_quit)
 
     def on_merge(self):
-        self._do_merge()
+        backup = self.window.config_bar.backup_check.is_checked()
+        self._do_merge(backup)
 
     def on_synthetize(self):
-        self._do_merge()
+        only_last_month = self.window.config_bar.last_month_check.is_checked()
+        backup = self.window.config_bar.backup_check.is_checked()
         self.window.action_bar.button_synthetize["state"] = tk.DISABLED
         self.window.action_bar.button_merge["state"] = tk.DISABLED
+
+        self._do_merge(backup)
 
         if self.thread is not None:
             self.thread.join()
 
-        self.thread = threading.Thread(target=self._do_synthetize)
+        self.thread = threading.Thread(
+            target=self._do_synthetize, args=(only_last_month,))
         self.thread.start()
 
     def on_quit(self):
         self.window.root.destroy()
 
-    def _do_synthetize(self):
+    def _do_synthetize(self, only_last_month: bool):
         # datetime.datetime.now().date()
         end_date = self.export_account.ended_at()
         # end_date - datetime.timedelta(days=30)
@@ -52,11 +58,13 @@ class App:
         month_scale = make_accounting_term_dates(start_date, end_date)
         self.window.progress_bar.step(amount=-100)
 
-        progress = 1/len(month_scale)*100
+        progress = 1 if only_last_month else 1/len(month_scale)*100
         render_synthesis(self.export_account, dates)
         self.window.progress_bar.step(amount=progress)
 
-        for start, end in zip(month_scale, month_scale[1:]):
+        all_periods = list(zip(month_scale, month_scale[1:]))
+        periods = [all_periods[-1]] if only_last_month else all_periods
+        for start, end in periods:
             dates = make_linear_date(start, end, days)
             render_synthesis(self.export_account, dates)
             self.window.progress_bar.step(amount=progress)
@@ -67,7 +75,7 @@ class App:
         self.window.action_bar.button_synthetize["state"] = tk.NORMAL
         self.window.action_bar.button_merge["state"] = tk.NORMAL
 
-    def _do_merge(self):
+    def _do_merge(self, backup: bool):
         try:
             import_filename = self.window.import_picker.filename()
             import_file = read_xls_file(import_filename)
@@ -78,12 +86,25 @@ class App:
             self.current_account = Account.parse_current(current_file)
 
         except Exception as e:
-            messagebox.showerror("Unable to open file", f"{e}")
+            traceback.print_exception(e)
+            messagebox.showerror("Unable to read files", f"{e}")
             return
 
-        self.export_account = self.current_account.merge(self.import_account)
-        export_rows = self.export_account.to_export()
-        write_csv_file(current_filename, export_rows)
+        try:
+            if backup:
+                backup_filename = current_filename + ".backup"
+                current_rows = self.current_account.to_export()
+                write_csv_file(backup_filename, current_rows)
+
+            self.export_account = self.current_account.merge(
+                self.import_account)
+            export_rows = self.export_account.to_export()
+
+            write_csv_file(current_filename, export_rows)
+        except Exception as e:
+            traceback.print_exception(e)
+            messagebox.showerror("Unable to write files", f"{e}")
+            return
 
         msg = f"Successfully merged {len(self.import_account.transactions)} transactions !"
         messagebox.showinfo("Done", msg)
--- gui.py
+++ gui.py
@@ -5,7 +5,7 @@ from tkinter import ttk
 
 
 class FilePickerFrame(tk.Frame):
-    filevar: tk.Variable
+    var_file: tk.Variable
     label: tk.Label
     entry: tk.Entry
     button: tk.Button
@@ -14,9 +14,10 @@ class FilePickerFrame(tk.Frame):
         super().__init__(root)
         cwd = os.getcwd()
         initial = initial if initial is not None else ""
-        self.filevar = tk.StringVar(root, value=f"{cwd}{os.path.sep}{initial}")
+        self.var_file = tk.StringVar(
+            root, value=f"{cwd}{os.path.sep}{initial}")
         self.label = ttk.Label(self, text=name,  width=20)
-        self.entry = ttk.Entry(self, textvariable=self.filevar)
+        self.entry = ttk.Entry(self, textvariable=self.var_file)
         self.button = ttk.Button(self, text="Select a file",
                                  command=self._select_file)
 
@@ -40,10 +41,40 @@ class FilePickerFrame(tk.Frame):
             filetypes=filetypes)
 
         if filename is not None and filename != "":
-            self.filevar.set(filename)
+            self.var_file.set(filename)
 
     def filename(self):
-        return self.filevar.get()
+        return self.var_file.get()
+
+
+class CheckFrame(tk.Frame):
+    var_check: tk.Variable
+    label: tk.Label
+    check: tk.Checkbutton
+
+    def __init__(self, root, name, intial=True):
+        super().__init__(root)
+        self.var_check = tk.BooleanVar(root, value=intial)
+        self.label = ttk.Label(self, text=name)
+        self.check = ttk.Checkbutton(self, variable=self.var_check)
+
+        self.label.pack(side=tk.LEFT)
+        self.check.pack(side=tk.RIGHT)
+
+    def is_checked(self):
+        return self.var_check.get()
+
+
+class ConfigurationBarFrame(tk.Frame):
+    last_month_check: CheckFrame
+    backup_check: CheckFrame
+
+    def __init__(self, root):
+        super().__init__(root)
+        self.last_month_check = CheckFrame(self, "Only last month")
+        self.backup_check = CheckFrame(self, "Backup current data")
+        self.last_month_check.pack(side=tk.LEFT)
+        self.backup_check.pack(side=tk.LEFT)
 
 
 class ActionBarFrame(tk.Frame):
@@ -67,13 +98,15 @@ class Window:
     progress_bar: ttk.Progressbar
     import_picker: FilePickerFrame
     current_picker: FilePickerFrame
+    config_bar: ConfigurationBarFrame
     action_bar: ActionBarFrame
 
     def __init__(self, on_merge, on_synthetize, on_quit):
         self.root = tk.Tk()
         self.root.title("Select a transactions record")
-        self.root.geometry("900x120")
-        self.root.resizable(width=False, height=False)
+
+        self.root.rowconfigure(0, weight=1)
+        self.root.columnconfigure(0, weight=1)
         self.root.protocol("WM_DELETE_WINDOW", on_quit)
 
         self.import_picker = FilePickerFrame(
@@ -84,9 +117,12 @@ class Window:
             self.root, "Current record")
         self.current_picker.pack(fill=tk.BOTH)
 
+        self.config_bar = ConfigurationBarFrame(self.root)
+        self.config_bar.pack()
+
         self.progress_bar = ttk.Progressbar(self.root, value=0)
         self.progress_bar.pack(fill=tk.BOTH)
 
         self.action_bar = ActionBarFrame(self.root, on_merge=on_merge,
                                          on_synthetize=on_synthetize)
-        self.action_bar.pack()
+        self.action_bar.pack(pady=10)
